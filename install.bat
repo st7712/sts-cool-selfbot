a454ff01a3099dea737cc88b3700effeaf10fc5d
imst <70844457+st7712@users.noreply.github.com>
2023-04-23 22:33:23 +0200

    installer update

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
-title st's selfbot requirements installer
-py -3.8 -m pip install -r requirements.txt
+title st's cool selfbot requirements installer
+py -3.8 -m pip install -r data/requirements.txt
 echo Finished!
-pause
+exit
